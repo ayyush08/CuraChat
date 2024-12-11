7d1cc0243712e0add49e9184237163d5d63e693c
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-11 10:44:23 +0530

    Refactor chat model and add message and request models

---
+++ server/models/message.model.js
@@ -0,0 +1,37 @@
+import mongoose, { models, Schema } from "mongoose";
+
+
+
+const messageSchema = new Schema({
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    chat:
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Chat',
+        required: true
+    },
+    content: {
+        type: String,
+    },
+    attachments: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+}, {
+    timestamps: true
+})
+
+
+export const Message = models.Message || mongoose.model('Message', messageSchema)
