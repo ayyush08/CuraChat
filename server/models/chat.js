6ad66280bf4598b5ad25ad188933a83695f29a2a
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-11 10:38:14 +0530

    Add Chat model with schema definition and timestamps

---
+++ server/models/chat.js
@@ -0,0 +1,29 @@
+import mongoose,{models, Schema} from "mongoose";
+
+
+
+const chatSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    groupChat:{
+        type:Boolean,
+        default:false
+    },
+    creator:{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    },
+    members:[
+        {
+            type: Schema.Types.ObjectId,
+            ref:'User'
+        }
+    ]
+},{
+    timestamps:true
+})
+
+
+export const Chat = models.Chat ||  mongoose.model('Chat',chatSchema)
