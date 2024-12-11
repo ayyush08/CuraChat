eb95e48db7666bc0f81f49b387994715ce28dc1d
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-11 10:35:08 +0530

    Set up Express server and implement user model

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
-console.log('Servesr is running');
+import express from 'express';
+
+const app = express();
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
