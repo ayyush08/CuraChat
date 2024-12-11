85cb6927e5550fef983d967b3bed315602b8da47
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-12-11 11:20:31 +0530

    add multer middleware for avatar upload in user registration

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { multerUpload } from "../middlewares/multer.middlerware.js";
 
 const router = Router();
 
-router.post('/register', registerUser);
+router.post('/register',
+    multerUpload.single('avatar')
+    , registerUser);
 
 
 export default router
