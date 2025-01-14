aad3f5d8b6603f27cfe8b4faed559a39167db121
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-01-14 22:25:05 +0530

    add routing and page components for navigation; include daisyui for styling

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -1,7 +1,26 @@
-export default function App() {
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+
+const App = () => {
   return (
-    <h1 className="text-9xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
+
+export default App
