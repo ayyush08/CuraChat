36ad2166bf8b193432d42b06e41a7f6d2d552a71
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-01-15 19:52:29 +0530

    add profile update functionality; implement image upload and display user information

--- mern/server/src/index.js
+++ mern/server/src/index.js
@@ -15,9 +15,9 @@ const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(cookieParser());
 app.use(
     cors({
@@ -25,6 +25,7 @@ app.use(
         credentials: true,
     })
 );
+
 
 app.use("/api/auth", authRoutes);
 // app.use("/api/messages", messageRoutes);
