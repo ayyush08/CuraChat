e4b3e0b9c11f32cb0e330bf984961ed573d064de
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-01-16 19:50:38 +0530

    add Sidebar, component

--- mern/server/src/index.js
+++ mern/server/src/index.js
@@ -8,7 +8,7 @@ import path from "path";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
-// import messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.route.js";
 // import { app, server } from "./lib/socket.js";
 const app = express();
 
@@ -28,7 +28,7 @@ app.use(
 
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 // if (process.env.NODE_ENV === "production") {
 //     app.use(express.static(path.join(__dirname, "../frontend/dist")));
