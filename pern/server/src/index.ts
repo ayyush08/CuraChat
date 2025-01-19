727cd50c54d20d6a01bc97435839cbb5c66dc5fa
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-01-19 13:29:03 +0530

    Prisma+NeonDB setup and Data modelling

--- pern/server/src/index.ts
+++ pern/server/src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+
 import authRoutes from '../src/routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 
-const app = express();
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -12,6 +17,6 @@ app.get('/', (req, res) => {
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes)
 
-app.listen(5000, () => {
-    console.log('Server  running on port 5000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
