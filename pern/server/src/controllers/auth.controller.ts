398c9a99904584d761548d5497cc9259a1595b54
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-01-19 16:14:16 +0530

    Login + logout+  enhance auth controller with type definitions and error handling

--- pern/server/src/controllers/auth.controller.ts
+++ pern/server/src/controllers/auth.controller.ts
@@ -1,11 +1,10 @@
-import { error, profile } from "console";
-import { Request, Response } from "express"
+import { Request, Response, RequestHandler } from "express"
 import prisma from "../db/prisma.js";
 import bcryptjs from 'bcryptjs'
 import generateToken from "../utils/generateToken.js";
 
 
-const signup = async (req: Request, res: Response) => {
+const signup: RequestHandler = async (req: Request, res: Response): Promise<any> => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body;
 
@@ -18,61 +17,92 @@ const signup = async (req: Request, res: Response) => {
         }
 
         const user = await prisma.user.findUnique({
-            where:{
+            where: {
                 username
             }
         })
 
-        if(user){
-            return res.status(400).json({error:"Username already exists"})
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" })
         }
 
         const salt = await bcryptjs.genSalt(10);
         const hashedPassword = await bcryptjs.hash(password, salt);
-        
+
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
         const newUser = await prisma.user.create({
-            data:{
+            data: {
                 fullName,
                 username,
-                password:hashedPassword,
+                password: hashedPassword,
                 gender,
                 profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
             }
         })
 
-        if(!newUser){
-            return res.status(500).json({error:"Something went wrong"})
+        if (!newUser) {
+            res.status(500).json({ error: "Something went wrong" })
         }
 
-        if(newUser){
+        if (newUser) {
             //generate token
             generateToken(newUser.id, res)
 
             res.status(200).json({
-                id:newUser.id,
-                fullName:newUser.fullName,
-                username:newUser.username,
-                profilePic:newUser.profilePic
+                id: newUser.id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
             })
-            
+
         }
-    } catch (error:any) {
-        console.log('Error in signup',error);
-        
+    } catch (error: any) {
+        console.log('Error in signup', error);
+        res.status(500).json({ error: "Something went wrong" })
     }
 }
 
-const login = async (req: Request, res: Response) => {
-    res.send('Hello login')
+const login = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({ where: { username } });
+
+        if (!user) {
+            return res.status(400).json({ error: "Invalid credentials" });
+        }
+
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ error: "Invalid credentials" });
+        }
+
+        generateToken(user.id, res);
+
+        res.status(200).json({
+            id: user.id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    } catch (error: any) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
 
 
-const logout = async (req: Request, res: Response) => {
-    res.send('Hello logout')
+const logout = async (req: Request, res: Response): Promise<any> => {
+    try {
+        res.cookie("curachat-pern-token", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error: any) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 }
 
-export { login, signup, logout }
+export { login, logout, signup }
--- pern/server/src/routes/auth.route.ts
+++ pern/server/src/routes/auth.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, signup,logout  } from "../controllers/auth.controller.js";
 
 const router = Router()
 
 router.post('/login',login)
-
 router.post('/signup',signup)
+
 
 router.post('/logout',logout)
 
